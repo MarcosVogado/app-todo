2416a593ffad5b505eacaa7c5541a221dd71879e
Marcos Vogado <marcoshcvogado@gmail.com>
2025-08-20 15:01:30 -0300

    add features, checked button

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+//interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+
+  //Declarando variáveis
+  public taskList: Array<TaskList> = [
+    {task: "Minha nova Task", checked: true},
+    {task: "Minha nova Task", checked: false}
+  ];
 
   constructor() { }
 
