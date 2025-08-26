83ba5707ec252ff76c7d002de89540fbf14237a3
Marcos Vogado <marcoshcvogado@gmail.com>
2025-08-26 14:48:16 -0300

    add function delete all and delete this item

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -21,4 +21,16 @@ export class TodoListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public deleteItemTaskList(event: number){
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Você deseja realmente deletar tudo?");
+    
+    if(confirm == true){
+      this.taskList = []
+    }
+  }
+
 }
